44d3721d773440b7fe021e9fc5576ba3d90200b9
Jonatas Pedroso <jonatasarlindo@hotmail.com>
2019-10-15 20:53:56 -0300

    Example Swagger

--- app/docs/controllers/test.js
+++ app/docs/controllers/test.js
@@ -1,13 +1,93 @@
 /**
 * @swagger
-* /api/hello:
+* /user:
 *   get:
 *     tags:
-*       - Test
-*     summary: Sample API
+*       - Users
+*     summary: Users Get
 *     parameters:
 *       - name: name
 *         description: Name of the user
+*         in: query
+*         required: false
+*         type: string
+*       - name: username
+*         description: Usuário
+*         in: query
+*         required: false
+*         type: string
+*     responses:
+*       200:
+*         description: Send hello message
+*         example:
+*           message: Hello Guess
+*/
+
+/**
+* @swagger
+* /user:
+*   post:
+*     tags:
+*       - Users
+*     summary: Users Post
+*     parameters:
+*       - name: name
+*         description: Name of the user
+*         in: query
+*         required: false
+*         type: string
+*       - name: username
+*         description: Usuário
+*         in: query
+*         required: false
+*         type: string
+*     responses:
+*       200:
+*         description: Send hello message
+*         example:
+*           message: Hello Guess
+*/
+
+/**
+* @swagger
+* /user/{user_id}:
+*   put:
+*     tags:
+*       - Users
+*     summary: Users Put
+*     parameters:
+*       - name: name
+*         description: Name of the user
+*         in: query
+*         required: false
+*         type: string
+*       - name: username
+*         description: Usuário
+*         in: query
+*         required: false
+*         type: string
+*     responses:
+*       200:
+*         description: Send hello message
+*         example:
+*           message: Hello Guess
+*/
+
+/**
+* @swagger
+* /user/{user_id}:
+*   delete:
+*     tags:
+*       - Users
+*     summary: Users Delete
+*     parameters:
+*       - name: name
+*         description: Name of the user
+*         in: query
+*         required: false
+*         type: string
+*       - name: username
+*         description: Usuário
 *         in: query
 *         required: false
 *         type: string
